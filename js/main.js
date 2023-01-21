6266f41ab7d68ad2024f348c14da3e13940ce697
Md.Atiqur Rahman <55776718+atiq9se@users.noreply.github.com>
2023-01-21 23:46:36 +0600

    all file add

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false
+
+});
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        // toggle nav
+        nav.classList.toggle('nav-active');
+
+        // animated links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+ 0.3}s`;
+            }
+        });
+        // burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+navSlide();
+
+$(window).scroll(function(event){
+    if($(document).scrollTop() < 1){
+    $('.header').removeClass('fixed');
+    $('.header').removeClass('active')
+    }else if($(document).scrollTop() >100){
+      $('.header').removeClass('fixed');
+      $('.header').addClass('active');
+      $('.menu_img').style.padding="10px";
+    }else{
+      $('.header').addClass('fixed');
+    }
+});
